4377cf665260220bb4612e726f8728cfdfa16e00
FilipKOZANY <98156476+FilipKOZANY@users.noreply.github.com>
2025-01-04 21:06:42 +0100

    Add files via upload

--- jumpadventure/menu.gd
+++ jumpadventure/menu.gd
@@ -11,7 +11,7 @@ func _on_start_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/options.tscn")
 	
 
 
