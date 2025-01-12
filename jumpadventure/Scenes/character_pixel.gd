bdb7b61b67fd14392f165ee210eb78ae73a0d0f2
FilipKOZANY <98156476+FilipKOZANY@users.noreply.github.com>
2025-01-12 11:30:40 +0100

    Add files via upload

--- jumpadventure/Scenes/character_pixel.gd
+++ jumpadventure/Scenes/character_pixel.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D  # Použití správného typu uzlu pro Godot 4.x
-
+class_name Player
 const TARGET_FPS = 60
 const ACCELERATION = 8.0
 const MAX_SPEED = 64.0
@@ -16,23 +16,21 @@ var motion = Vector2.ZERO  # Inicializace pohybu
 
 func _physics_process(delta):
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	
+
 	# Horizontální pohyb
 	if x_input != 0:
 		motion.x += x_input * ACCELERATION * delta * TARGET_FPS
 		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
 		sprite.flip_h = x_input < 0  # Otočení sprite, pokud je pohyb vlevo
 		if is_on_floor():
-			if not animation_player.is_playing() or animation_player.current_animation != "Run":
-				animation_player.play("Run")  # Spuštění animace "Run"
+			animation_player.play("Run")  # Spuštění animace "Run"
 	else:
 		if is_on_floor():
 			motion.x = lerp(motion.x, 0.0, FRICTION * delta)
-			if not animation_player.is_playing() or animation_player.current_animation != "Stand":
-				animation_player.play("Stand")  # Spuštění animace "Stand"
+			animation_player.play("Stand")  # Spuštění animace "Stand"
 		else:
 			motion.x = lerp(motion.x, 0.0, AIR_RESISTANCE * delta)
-	
+
 	# Vertikální pohyb
 	motion.y += GRAVITY * delta * TARGET_FPS
 
@@ -43,15 +41,12 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_select"):  # Skákání pomocí "ui_up" nebo "Space"
 			motion.y = -JUMP_FORCE
-			if not animation_player.is_playing() or animation_player.current_animation != "Jump":
-				animation_player.play("Jump")  # Spuštění animace "Jump"
+			animation_player.play("Jump")  # Spuštění animace "Jump"
 	else:
 		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE / 2:
 			motion.y = -JUMP_FORCE / 2
-		if not is_on_floor():
-			if not animation_player.is_playing() or animation_player.current_animation != "Jump":
-				animation_player.play("Jump")  # Spuštění animace "Jump"
-	
+		animation_player.play("Fall")  # Spuštění animace "Fall", pokud není na zemi
+
 	# Pohyb
 	velocity = motion  # Aktualizace velocity pro CharacterBody2D
 	move_and_slide()
