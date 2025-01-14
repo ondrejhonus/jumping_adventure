c740c7cfc5cd130ae8944443065c9c4d7a73bd3b
Ondrej Honus - Laptop <ondrejhonus@proton.me>
2025-01-14 15:57:01 +0100

    fix dependencies on file management fix

--- menu.gd
+++ menu.gd
@@ -3,9 +3,6 @@ extends Control
 func _ready() -> void:
 	$VBoxContainer/START.grab_focus()
 
-
-
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/LevelOne.xml.tscn")
 
